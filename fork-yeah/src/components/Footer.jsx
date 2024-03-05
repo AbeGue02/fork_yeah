c818d16ca2291aacf488d2cc3decca02127f7b78
toddlf705 <toddlf705@gmail.com>
2024-03-05 17:27:02 -0500

    add footer home and main components

--- fork-yeah/src/components/Footer.jsx
+++ fork-yeah/src/components/Footer.jsx
@@ -1,7 +1,36 @@
 export default function Footer () {
     return (
-        <>
-        
-        </>
+        <div className="Footer">
+            <div className="github">
+                <h5><a href="https://github.com/AbeGue02/fork_yeah">Github</a></h5>
+                <ul>
+                    <li><button>button</button></li>
+                </ul>
+            </div>
+
+            <div className="github">
+                <h5>Abraham Guerrero</h5>
+                <ul>
+                    <li><a href="https://github.com/AbeGue02">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/abrahamdguerrero/">LinkedIn</a></li>
+                </ul>
+            </div>
+
+            <div className="github">
+                <h5>Felix Man</h5>
+                <ul>
+                    <li><a href="https://github.com/felixm126">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/felix-man/">LinkedIn</a></li>
+                </ul>
+            </div>
+
+            <div className="github">
+                <h5>Daisy Ban</h5>
+                <ul>
+                    <li><a href="https://github.com/toddlf705">Github</a></li>
+                    <li><a href="https://www.linkedin.com/in/daisy-ban/">LinkedIn</a></li>
+                </ul>
+            </div>
+        </div>
     )
-}
+} 
--- fork-yeah/src/components/Home.jsx
+++ fork-yeah/src/components/Home.jsx
@@ -1,7 +1,12 @@
+import home_img from '../assets/home.png'
+
 export default function Home () {
     return (
         <>
-        
+        <div className="container">
+            <div className="categories"> Map Categories Here </div>
+            <img src= {home_img} width="1800px"></img>
+        </div>
         </>
     )
 }
