bed208c787bad7485d922f90efa1eb505b1ad5e1
Abraham Guerrero <135165898+AbeGue02@users.noreply.github.com>
2024-03-06 17:00:24 -0500

    Revert "7 create list and list item components"

--- fork-yeah/src/components/Header.jsx
+++ fork-yeah/src/components/Header.jsx
@@ -1,22 +1,7 @@
-import { useNavigate } from 'react-router-dom'
-import SearchBar from './SearchBar'
-import '../App.css'
-
-export default function Header() {
-	const navigate = useNavigate()
-
-	return (
-		<div>
-			<header className='horizontal-flex'>
-				<h1>Fork Yeah!</h1>
-				<SearchBar />
-			</header>
-			<nav>
-				<ul>
-					<li onClick={() => navigate('/')}>Home</li>
-					<li onClick={() => navigate('/meal')}>Surprise Me!</li>
-				</ul>
-			</nav>
-		</div>
-	)
+export default function Header () {
+    return (
+        <>
+        
+        </>
+    )
 }
