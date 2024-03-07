d3f26a9624f4fe86f9090966f29cdd07484f6214
Abraham Guerrero <135165898+AbeGue02@users.noreply.github.com>
2024-03-07 14:34:59 -0500

    Revert "Merging Dev"

--- fork-yeah/src/components/Footer.jsx
+++ fork-yeah/src/components/Footer.jsx
@@ -1,33 +1,7 @@
-import GitHubButton from 'react-github-btn'
-
 export default function Footer () {
     return (
-        <footer>
-            <div className="github">
-                <h4>Github</h4>
-                <GitHubButton href="https://github.com/AbeGue02/fork_yeah" data-color-scheme="no-preference: light; light: light; dark: dark;" data-icon="octicon-star" data-size="large" aria-label="Star AbeGue02/fork_yeah on GitHub">Star</GitHubButton>
-            </div>
-            <div className="github">
-                <h4>Abraham Guerrero</h4>
-                <ul>
-                    <li><GitHubButton href="https://github.com/AbeGue02" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Follow @AbeGue02 on GitHub">Follow @AbeGue02</GitHubButton></li>
-                    <li><a href="https://www.linkedin.com/in/abrahamdguerrero/">LinkedIn</a></li>
-                </ul>
-            </div>
-            <div className="github">
-                <h4>Felix Man</h4>
-                <ul>
-                    <li><GitHubButton href="https://github.com/felixm126" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Follow @felixm126 on GitHub">Follow @felixm126</GitHubButton></li>
-                    <li><a href="https://www.linkedin.com/in/felix-man/">LinkedIn</a></li>
-                </ul>
-            </div>
-            <div className="github">
-                <h4>Daisy Ban</h4>
-                <ul>
-                    <li><GitHubButton href="https://github.com/toddlf705" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Follow @toddlf705 on GitHub">Follow @toddlf705</GitHubButton></li>
-                    <li><a href="https://www.linkedin.com/in/daisy-ban/">LinkedIn</a></li>
-                </ul>
-            </div>
-        </footer>
+        <>
+        
+        </>
     )
 }
--- fork-yeah/src/components/Header.jsx
+++ fork-yeah/src/components/Header.jsx
@@ -1,22 +1,7 @@
-import { useNavigate } from 'react-router-dom'
-import SearchBar from './SearchBar'
-import '../App.css'
-
-export default function Header() {
-	const navigate = useNavigate()
-
-	return (
-		<div>
-			<header>
-				<h1>Fork Yeah!</h1>
-				<SearchBar />
-			</header>
-			<nav>
-				<ul>
-					<li onClick={() => navigate('/')}>Home</li>
-					<li onClick={() => navigate('/meals/random')}>Surprise Me!</li>
-				</ul>
-			</nav>
-		</div>
-	)
+export default function Header () {
+    return (
+        <>
+        
+        </>
+    )
 }
