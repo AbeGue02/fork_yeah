eff8545299fb7a97b967245d4d30015dc6059560
AbeGue02 <abrahamgue02@gmail.com>
2024-03-07 15:02:19 -0500

    Fixes bad changes in branch

--- fork-yeah/src/components/Home.jsx
+++ fork-yeah/src/components/Home.jsx
@@ -4,6 +4,25 @@ import axios from 'axios'
 import LoadingSpinner from "./LoadingSpinner"
 
 export default function Home () {
+    
+    const [categories, setCategories] = useState([])
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const getCategories = async () => {
+            let response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+            //console.log(response.data.categories)
+            setCategories(response.data.categories)
+        }
+
+        getCategories()
+    }, [])
+
+    useEffect(() => {
+        console.log(categories)
+    }, [categories])
+    
     return (
         <div className="homeScreen">
             
